6bc39fcaee119cdb28929cd92596fa6de9f13c58
Arne Nordmann <arne.nordmann@de.bosch.com>
2020-08-03 14:11:49 +0200

    flake
    Signed-off-by: Arne Nordmann <arne.nordmann@de.bosch.com>

--- system_modes/launch/mode_monitor.launch.py
+++ system_modes/launch/mode_monitor.launch.py
@@ -21,28 +21,28 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-      launch.actions.DeclareLaunchArgument(
-        'modelfile',
-        description='Path to modelfile'),
-      launch.actions.DeclareLaunchArgument(
-        'debug',
-        default_value='false',
-        description='Debug'),
-      launch.actions.DeclareLaunchArgument(
-        'verbose',
-        default_value='false',
-        description='Print mode parametrization'),
-      launch.actions.DeclareLaunchArgument(
-        'rate',
-        default_value='1000',
-        description='Monitor refresh rate in ms'),
-      launch_ros.actions.Node(
-        package='system_modes',
-        executable='mode_monitor',
-        parameters=[{
-            'modelfile': launch.substitutions.LaunchConfiguration('modelfile'),
-            'debug': launch.substitutions.LaunchConfiguration('debug'),
-            'verbose': launch.substitutions.LaunchConfiguration('verbose'),
-            'rate': launch.substitutions.LaunchConfiguration('rate')
-            }],
-        output='screen')])
+        launch.actions.DeclareLaunchArgument(
+            'modelfile',
+            description='Path to modelfile'),
+        launch.actions.DeclareLaunchArgument(
+            'debug',
+            default_value='false',
+            description='Debug'),
+        launch.actions.DeclareLaunchArgument(
+            'verbose',
+            default_value='false',
+            description='Print mode parametrization'),
+        launch.actions.DeclareLaunchArgument(
+            'rate',
+            default_value='1000',
+            description='Monitor refresh rate in ms'),
+        launch_ros.actions.Node(
+            package='system_modes',
+            executable='mode_monitor',
+            parameters=[{
+                'modelfile': launch.substitutions.LaunchConfiguration('modelfile'),
+                'debug': launch.substitutions.LaunchConfiguration('debug'),
+                'verbose': launch.substitutions.LaunchConfiguration('verbose'),
+                'rate': launch.substitutions.LaunchConfiguration('rate')
+                }],
+            output='screen')])

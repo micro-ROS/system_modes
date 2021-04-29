edb7b2025868e5b0edf77c92b1ed654e73aa2e21
Nordmann Arne (CR/ADT3) <arne.nordmann@de.bosch.com>
2021-04-29 14:23:21 +0200

    Adds example launch file that changes modes
    Signed-off-by: Nordmann Arne (CR/ADT3) <arne.nordmann@de.bosch.com>

--- system_modes_examples/launch/example_system_started.launch.py
+++ system_modes_examples/launch/example_system_started.launch.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import sys
+
 import ament_index_python.packages
 
 import launch
@@ -28,10 +29,11 @@ import launch_ros.events
 import launch_ros.events.lifecycle
 
 import launch_system_modes.actions
-import launch_system_modes.events
 import launch_system_modes.event_handlers
+import launch_system_modes.events
 
 import lifecycle_msgs
+
 
 def generate_launch_description():
     modelfile = (ament_index_python.packages.get_package_share_directory('system_modes_examples') +
@@ -44,10 +46,6 @@ def generate_launch_description():
                 ament_index_python.packages.get_package_share_directory(
                     'system_modes') + '/launch/mode_manager.launch.py'),
             launch_arguments={'modelfile': modelfile}.items())
-
-        system = launch_system_modes.actions.System(
-                name='sys',
-                namespace='')
 
         drive_base = launch_system_modes.actions.SystemPart(
                 package='system_modes_examples',
@@ -64,11 +62,6 @@ def generate_launch_description():
             launch_arguments={'modelfile': modelfile}.items())
 
         # Startup
-        system_configure = launch.actions.EmitEvent(
-            event=launch_ros.events.lifecycle.ChangeState(
-                lifecycle_node_matcher=launch.events.matchers.matches_action(system),
-                transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE))
-
         drive_base_configure = launch.actions.EmitEvent(
             event=launch_ros.events.lifecycle.ChangeState(
                 lifecycle_node_matcher=launch.events.matchers.matches_action(drive_base),
@@ -79,21 +72,10 @@ def generate_launch_description():
                 lifecycle_node_matcher=launch.events.matchers.matches_action(drive_base),
                 transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
             ))
-
-        system_change_mode_to_DEFAULT = launch.actions.EmitEvent(
-            event=launch_system_modes.events.ChangeMode(
-                lifecycle_node_matcher=launch.events.matchers.matches_action(system),
-                mode_name='__DEFAULT__',
-            ))
         drive_base_change_mode_to_DEFAULT = launch.actions.EmitEvent(
             event=launch_system_modes.events.ChangeMode(
                 lifecycle_node_matcher=launch.events.matchers.matches_action(drive_base),
                 mode_name='__DEFAULT__',
-            ))
-        drive_base_change_mode_to_FAST = launch.actions.EmitEvent(
-            event=launch_system_modes.events.ChangeMode(
-                lifecycle_node_matcher=launch.events.matchers.matches_action(drive_base),
-                mode_name='FAST',
             ))
 
         # Handlers
@@ -102,26 +84,20 @@ def generate_launch_description():
                 target_lifecycle_node=drive_base,
                 goal_state='inactive',
                 entities=[drive_base_activate]))
+
         on_active_handler = launch.actions.RegisterEventHandler(
             launch_ros.event_handlers.OnStateTransition(
                 target_lifecycle_node=drive_base,
                 goal_state='active',
-                entities=[system_change_mode_to_DEFAULT]))
-        on_FAST_mode = launch.actions.RegisterEventHandler(
-            launch_system_modes.event_handlers.OnModeChanged(
-                target_system_part=drive_base,
-                goal_mode='__DEFAULT__',
-                entities=[drive_base_change_mode_to_FAST]))
+                entities=[drive_base_change_mode_to_DEFAULT]))
 
         description = launch.LaunchDescription()
         description.add_action(on_inactive_handler)
-        #description.add_action(on_active_handler)
-        #description.add_action(on_FAST_mode)
+        description.add_action(on_active_handler)
         description.add_action(mode_manager)
         description.add_action(drive_base)
         description.add_action(manipulator)
         description.add_action(drive_base_configure)
-        #description.add_action(drive_base_change_mode_to_FAST)
     except TypeError as err:
         print(err)
         print(sys.exc_type)
@@ -129,7 +105,7 @@ def generate_launch_description():
         print(err)
         print(sys.exc_type)
     except AttributeError as err:
-        print(Attributerr)
+        print(err)
         print(sys.exc_type)
     except Exception as err:
         exception_type = type(err).__name__
